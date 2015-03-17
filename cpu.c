dc1eaeecda89a161ef57479a6b26450df053ebee
Gordon <geniusgordon@gmail.com>
2015-03-17 14:29:22 +0800

    make ins and mem to 1024

--- cpu.c
+++ cpu.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "include/cpu.h"
@@ -6,18 +7,37 @@ struct cpu_struct* alloc_cpu()
 {
 	struct cpu_struct* cpu = (struct cpu_struct*)malloc(sizeof(struct cpu_struct));
 	memset(cpu, 0, sizeof(struct cpu_struct));
-	cpu->pc = get_ins(&cpu->ins);
+	cpu->pc = init_mem(cpu->ins, 'i');
+//	init_mem(cpu->mem, 'd');
 	return cpu;
 }
 
 void free_cpu(struct cpu_struct* cpu)
 {
-	if (!cpu->ins)
-		free(cpu->ins);
-	if (!cpu->mem)
-		free(cpu->mem);
-	if (!cpu->current_ins)
+	if (cpu->current_ins)
 		free(cpu->current_ins);
 	free(cpu);
+}
+
+void decode(struct cpu_struct* cpu)
+{
+	struct ins_struct* dec = (struct ins_struct*)malloc(sizeof(struct ins_struct));
+	dec->ins = cpu->ins[cpu->pc];
+	dec->op = extract(dec->ins, 31, 26);
+	dec->rs = extract(dec->ins, 25, 21);
+	dec->rt = extract(dec->ins, 20, 16);
+	dec->rd = extract(dec->ins, 15, 11);
+	dec->shamt = extract(dec->ins, 10, 6);
+	dec->funct = extract(dec->ins, 5, 0);
+	dec->imm = extract(dec->ins, 15, 0);
+	dec->addr = extract(dec->ins, 25, 0);
+        cpu->current_ins = dec;
+}
+
+void execute(struct cpu_struct* cpu)
+{
+	printf("%08x\n", cpu->current_ins->ins);
+        printf("%x\n", cpu->current_ins->op);
+        printf("%x\n", cpu->current_ins->addr);
 }
 
--- include/cpu.h
+++ include/cpu.h
@@ -6,12 +6,14 @@
 struct cpu_struct {
 	word_t pc;
 	word_t reg[32];
-	word_t* ins;
-	word_t* mem;
+	word_t ins[1024];
+	word_t mem[1024];
 	struct ins_struct* current_ins;
 };
 
 struct cpu_struct* alloc_cpu();
 void free_cpu(struct cpu_struct* cpu);
+void decode(struct cpu_struct* cpu);
+void execute(struct cpu_struct* cpu);
 
 #endif
